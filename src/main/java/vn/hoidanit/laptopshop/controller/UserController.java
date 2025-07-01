d9db6a15568cc52b75b3780ad377679f370b3b6f
Đoàn Anh Nhật <nhatda@dc1.gemvietnam.com>
2025-07-01 17:24:34 +0700

    Update code

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,26 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+@RestController
+public class UserController {
+    private UserService userService;
+    
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("")
+    public String getHomePage() {
+        return "home.html";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage() {
+        return "admin";
+    }
+}
