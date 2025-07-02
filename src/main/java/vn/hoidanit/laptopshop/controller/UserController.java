621f9f4efc9c3acdfd80b0cd5654041346c1e07f
Đoàn Anh Nhật <nhatda@dc1.gemvietnam.com>
2025-07-02 17:14:02 +0700

    Update code

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,16 +1,21 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @RestController
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
     
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
     }
 
@@ -19,8 +24,10 @@ public class UserController {
         return "home.html";
     }
 
-    @RequestMapping("/admin/user")
-    public String getUserPage() {
+    @RequestMapping(value = "/admin/user", method = RequestMethod.POST)
+    public String createUserPage(@RequestBody User user) {
+        System.out.println("nhat" + user);
+        this.userService.handleSaveUser(user);
         return "admin";
     }
 }
