ac344f13de7ee7cf6b92d4dc42e0cdb27605f696
Đoàn Anh Nhật <nhatda@dc1.gemvietnam.com>
2025-07-11 10:49:57 +0700

    Update code

--- src/main/java/vn/hoidanit/laptopshop/domain/Product.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Product.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,7 @@ public class Product {
     private Long sold;
     private String factory;
     private String target;
+
     public Long getId() {
         return id;
     }
